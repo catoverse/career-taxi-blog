9efef7fed577080ab3b44c19e71ceca40b921741
ashwindevx <ashwinchauhan.developer@gmail.com>
2021-09-17 15:40:47 +0530

    styling changes

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,13 @@
 import Image from "next/image";
-// import { useRouter } from "next/router";
 import styles from "./Header.module.css";
-import logo from "../assets/logo.svg";
+import logo from "../public/logo/logo.png";
 
 export const Header = () => {
   return (
     <div className={styles.main}>
       <div className={styles.wrapper}>
         <a href="https://www.career.taxi/">
-          <Image src={logo} alt="Career Taxi Logo" />
+          <Image src={logo} alt="career taxi logo" width="50px" height="50px" />
         </a>
         <a href="mailto:rutvij.karkhanis@gmail.com">Email</a>
       </div>
